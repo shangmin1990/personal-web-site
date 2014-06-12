cffd54bb9a90bd87ba0d251358cafa31a5608690
Benjamin <shangmin@goodow.com>
2014-06-12 16:09:21 +0800

    module article archetype complete

--- js/app.js
+++ js/app.js
@@ -5,5 +5,59 @@ angular.module("website",[
   'website.directive',
   'website.service'
 ]).config(['$stateProvider','$urlRouterProvider',function($stateProvider,$urlRouterProvider){
-  $stateProvider.state()
+  $stateProvider.state('home',{
+    url: "/"
+  });
+  /**
+   * module article $urlRouter config begin
+   */
+
+  //TODO
+
+  /**
+   * module article $state config begin
+   */
+  $stateProvider.state('article',{
+    url: "/article",
+    //mean 此状态不能被单独激活.只能由其子状态激活
+    abstract:"true",
+    templateUrl:'template/article/article.html'
+  });
+  ////////////////////
+  ///article > all///
+  //////////////////
+  $stateProvider.state('all',{
+    url: "/all",
+    parent:'article',
+    templateUrl:'template/article/article.all.html'
+  });
+  ////////////////////
+  ///article > type///
+  //////////////////
+  $stateProvider.state('type',{
+    url: "/type",
+    parent:'article',
+    templateUrl:'template/article/article.type.html'
+  });
+  ////////////////////
+  ///article > trash///
+  //////////////////
+  $stateProvider.state('trash',{
+    url: "/trash",
+    parent:'article',
+    templateUrl:'template/article/article.all.html'
+  });
+  ////////////////////
+  ///article > newArticle///
+  //////////////////
+  $stateProvider.state('newArticle',{
+    url: "/newArticle",
+    parent:'article',
+    templateUrl:'template/article/article.newArticle.html'
+  });
+  //module article config end
+
+  $stateProvider.state('photo',{
+    
+  });
 }]);
---
+++ js/service.js
@@ -0,0 +1,4 @@
+/**
+ * Created by benjamin on 14-6-12.
+ */
+angular.module("website.service",[])
